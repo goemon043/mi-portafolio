a854acb1055bbe67a40229ea3d0cee05594adf84
goemon043 <goemon043@gmail.com>
2025-11-29 08:37:35 -0400

    se agrego imagenes, estilos con el span, correccion de un mensaje de js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 alert("Gracias por entrar a mi página");
 document.querySelector('h1').addEventListener('click', () => {
-    alert("¡Qué curioso que eres!");
+    alert("Que curioso que eres");
 });
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     alert("¡Gracias por enviar tus datos!");
 });
+
